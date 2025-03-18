c2e7cfaea2b90b097b05cca41fcef12a9bd0d089
jarturomora <jarturomora@gmail.com>
2025-03-18 10:29:40 +0100

    Agrega hola-js a Tema 3

---
+++ temas/tema-3/code/hola-js/scripts/funciones.js
@@ -0,0 +1,5 @@
+function mostrarFechaHora() {
+  const fechaHora = new Date();
+  const formato = fechaHora.toLocaleString();
+  document.getElementById("fechaHora").textContent = `Fecha y hora actual: ${formato}`;
+}
